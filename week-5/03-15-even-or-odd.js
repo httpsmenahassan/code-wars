57478f7a41ab287fa1695c925b9204832237694c
httpsmenahassan <125938686+httpsmenahassan@users.noreply.github.com>
2023-03-19 22:48:39 -0400

    Delete week-5 directory

--- week-5/03-15-even-or-odd.js
+++
@@ -1,9 +0,0 @@
-// Day 3 -- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-
-function evenOrOdd(number) {
-  if(number % 2 === 0) {
-    return 'Even';
-  } else {
-    return 'Odd';
-  }
-}
