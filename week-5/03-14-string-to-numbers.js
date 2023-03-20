d7d4304690a92e405f4d85d464740a07f4155926
httpsmenahassan <125938686+httpsmenahassan@users.noreply.github.com>
2023-03-19 22:40:37 -0400

    Add files via upload

---
+++ week-5/03-14-string-to-numbers.js
@@ -0,0 +1,21 @@
+// Day 2 - We need a function that can transform a string into a number. What ways of achieving this do you know?
+// Did this together with mentor Mark, Zen, Jessica, and Vons
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+function stringToNumber(str) {
+    return Number(str);
+  }
+  
+ //cool kids way that Vonds introduced to me
+  function stringToNumber(str) {
+    return +str;
+  }
+  
+  const stringToNumber = function(str){
+    return Number(str);
+  }
+  
+  function stringToNumber(str) {
+    const result = Number(str)
+    return result
+  }
---
+++ week-5/03-17-reverse.js
@@ -0,0 +1,7 @@
+// day 5 - did this with Vonds during their review session
+
+// Complete the solution so that it reverses the string passed into it.
+
+const reverseStr = (string) => {
+    return string.split('').reverse().join('')
+}
