16949345bdd452c9959bbac4a4af097e15e49a78
httpsmenahassan <whobutmenacodes@gmail.com>
2023-03-23 17:37:16 -0400

    daily coding challenge

--- 03-14-string-to-numbers.js
+++ 03-14-string-to-numbers.js
@@ -1,5 +1,5 @@
 // Day 2 - We need a function that can transform a string into a number. What ways of achieving this do you know?
-// Did this together with mentor Mark, Zen, Jessica, and Vons
+// Did this together with mentor Mark, Zen, Jessica, and Vonds
 // Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
 function stringToNumber(str) {
