437c6cf5a0a887feba5aa10c39431aad98d066a8
httpsmenahassan <whobutmenacodes@gmail.com>
2023-04-03 21:56:22 -0400

    updated to include that I worked with mentor Mark on this

--- week-8/04-03-sum-of-differences-in-array.js
+++ week-8/04-03-sum-of-differences-in-array.js
@@ -1,6 +1,7 @@
 // Your task is to sum the differences between consecutive pairs in the array in descending order.
 // In descending order: [10, 2, 1]
 // Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+// worked on this with mentor Mark
 
 function sumOfDifferences(arr) {
     let sum = 0
