8097c510947ef307f81da2ffcbab708de0d0ff6d
httpsmenahassan <whobutmenacodes@gmail.com>
2023-04-23 18:49:51 -0400

    updated syntax

--- 03-15-even-or-odd.js
+++ 03-15-even-or-odd.js
@@ -1,7 +1,7 @@
 // Day 3 -- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function evenOrOdd(number) {
-  if(number % 2 === 0) {
+  if (number % 2 === 0) {
     return 'Even';
   } else {
     return 'Odd';
