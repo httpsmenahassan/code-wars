c6a82ed2122ed76dbe4da6d7d9ae74f7ef1d0c9d
httpsmenahassan <whobutmenacodes@gmail.com>
2023-05-15 23:38:04 -0400

    daily coding challenge

---
+++ week-14/05-15-most-likely.js
@@ -0,0 +1,16 @@
+// Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+
+// For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+// So:
+
+// Input: ('1:3','1:2') - returns false as 1 in 3 is less likely than 1 in 2.
+
+// yoinked this destructuring solution that I liked
+
+function mostLikely(prob1, prob2) {
+    const [a, b] = prob1.split(':')
+    const [c, d] = prob2.split(':')
+
+    return a / b > c / d
+}
