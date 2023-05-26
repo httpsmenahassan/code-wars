22c20cbf87544dfb71ebcb4b1e877c3543e84cc0
httpsmenahassan <whobutmenacodes@gmail.com>
2023-05-25 23:52:55 -0400

    editing formatting

--- 03-13-string-true-or-false.js
+++ 03-13-string-true-or-false.js
@@ -1,16 +1,9 @@
 // Day 1 - Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
-function boolToWord( bool ){
-    // create condtion if true and return yes
-    if ((bool === true)) return('Yes')
-    // create else to return no if false
-    else return('No')
+function boolToWord(bool){
+  if (bool === true) {
+    return('Yes')
+  } else {
+    return ('No')
   }
-
-
-
-const stringToNumber = (str) => +str
-
-
-
-
+}
